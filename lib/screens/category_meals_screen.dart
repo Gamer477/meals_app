4dce4e55f085a4f41d6a092bd7d44685f12e14ff
Youssef Mohamed <38445766+Gamer477@users.noreply.github.com>
2021-10-19 17:20:27 +0200

    new one

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String? categoryTitle;
+  List<Meal>? displayedMeals;
+  var _loadedInData = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInData) {
+      final routeArgs =
+          ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInData = true;
+    }
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals!.removeWhere((meal) {
+        return meal.id == mealId;
+      });
+    });
+  }
+
+  // final String categoryId;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemCount: displayedMeals!.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: displayedMeals![index].id,
+            title: displayedMeals![index].title,
+            duration: displayedMeals![index].duration,
+            affordability: displayedMeals![index].affordability,
+            complexity: displayedMeals![index].complexity,
+            imageUrl: displayedMeals![index].imageUrl,
+            removeItem: _removeMeal,
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class FilterScreen extends StatefulWidget {
+  static const routeName = '/filters';
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _gultenFree = false;
+  var _vegetrian = false;
+  var _vegan = false;
+  var _lactosFree = false;
+
+  Widget _buildSwitchListTile(
+      String title, String description, var currentValue, var updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust Your meal Selection',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile(
+                  'Gluten-free',
+                  'Only Include Gluten-free meals',
+                  _gultenFree,
+                  (newvalue) {
+                    setState(() {
+                      _gultenFree = newvalue;
+                    });
+                  },
+                ),
+                _buildSwitchListTile('Lactos-free',
+                    'Only Include Lactose-free meals', _lactosFree, (newvalue) {
+                  setState(() {
+                    _lactosFree = newvalue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Vegetrian', 'Only Include Vegetrian meals', _vegetrian,
+                    (newvalue) {
+                  setState(() {
+                    _vegetrian = newvalue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Vegan', 'Only Include Vegan meals', _vegan, (newvalue) {
+                  setState(() {
+                    _vegan = newvalue;
+                  });
+                })
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
